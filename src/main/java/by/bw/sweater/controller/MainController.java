0741b30dd25b7a8d48562e3a51f1956b0d07d8e2
gorguls <45212976+gorguls@users.noreply.github.com>
2021-11-16 20:39:57 +0300

    5 урок: Подключаем шаблонизатор FREEMARKER

--- src/main/java/by/bw/sweater/controller/MainController.java
+++ src/main/java/by/bw/sweater/controller/MainController.java
@@ -3,8 +3,10 @@ import by.bw.sweater.domain.Message;
 import by.bw.sweater.domain.User;
 import by.bw.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,14 +20,23 @@ public class MainController {
     public MessageRepo messageRepo;
 
     @GetMapping("/")
-    public String greeting( Map<String, Object> model) {
+    public String greeting( Map<String, Object> model ) {
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter
+            ,Model model) {
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -40,20 +51,7 @@ public class MainController {
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-        return "main";
+        return "redirect:/main"; // в уроке это не описано
+//        return "main";
     }
 }
